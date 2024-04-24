d125b521d62f7cbbd20959079e3de672a610c8cb
azhari-bizapp <azhari@bizapp.my>
2024-04-24 11:19:55 +0800

    dev: renew status project

---
+++ lib/env.dart
@@ -0,0 +1,15 @@
+import 'dart:core';
+
+class Env{
+  static String baseUrl = "https://corrad.visionice.net/bizapp/apigenerator_VERSIX.php?";
+
+  static String versi = "2.9.1";
+
+  static String cariapa = "abc280801zz";
+
+  static String get loginurl => "${baseUrl}api_name=TRACK_LOGIN";
+
+  static String get statisticurl => "${baseUrl}api_name=TRACK_GET_STATISTICS&TX=";
+
+  static String get rekodurl => "${baseUrl}api_name=TRACK_LIST_DONETRACKINGNO&TX=";
+}
