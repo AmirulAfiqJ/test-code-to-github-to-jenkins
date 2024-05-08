72f2d510c2b87f176c0c2597ed99f2842498b715
Engku Nazurah <engkunazurah7@gmail.com>
2024-05-08 10:20:13 +0800

    no message

--- lib/ui/status.dart
+++ lib/ui/status.dart
@@ -1,8 +1,10 @@
 import 'package:bizapptrack/ui/button.dart';
 import 'package:bizapptrack/ui/data_user.dart';
 import 'package:bizapptrack/env.dart';
+import 'package:bizapptrack/ui/home.dart';
 import 'package:bizapptrack/ui/list_to_excel.dart';
 import 'package:bizapptrack/ui/loading_widget.dart';
+// import 'package:bizapptrack/ui/sideNav.dart';
 import 'package:bizapptrack/viewmodel/status_viewmodel.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -16,166 +18,206 @@ class TestRenew extends StatefulWidget {
 }
 
 class _TestRenewState extends State<TestRenew> {
-
   TextEditingController usernameController = TextEditingController();
+  // final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xfff0f8ff),
-      floatingActionButton:  BizappButton(
+      // key: _scaffoldKey, // Add this line to assign the scaffold key
+      backgroundColor: Color.fromARGB(255, 255, 255, 255),
+      floatingActionButton: BizappButton(
         color: Colors.black87,
         title: "Export Excel",
-        tapCallback: ()=> Navigator.push(context, MaterialPageRoute(builder: (context)=> const ListToExcel())),
+        tapCallback: () => Navigator.push(context,
+            MaterialPageRoute(builder: (context) => const ListToExcel())),
       ),
-
       appBar: AppBar(
-        backgroundColor: Colors.transparent,
+        backgroundColor: Colors.black,
+        // leading: IconButton(
+        //   icon: const Icon(Icons.menu, color: Colors.white),
+        //   onPressed: () {
+        //     _scaffoldKey.currentState!.openDrawer();
+        //   },
+        // ),
+        leading: IconButton(
+    icon: Icon(
+      Icons.arrow_back,
+      color: Colors.white, // Customize the color here
+    ),
+    onPressed: () {
+      Navigator.push(context,
+            MaterialPageRoute(builder: (context) =>  HomePage()));
+    },
+  ),
         actions: [
-
           Padding(
             padding: const EdgeInsets.only(right: 20),
-            child: Text(Env.versi, style: const TextStyle(
-              color: Colors.black
-            )),
+            child: Text(Env.versi, style: const TextStyle(color: Colors.black)),
           )
         ],
       ),
-
       body: LayoutBuilder(
-        builder: (context, constraints)=> Consumer<StatusController>(
+        builder: (context, constraints) => Consumer<StatusController>(
           builder: (context, model, child) {
             return SingleChildScrollView(
               child: Center(
                 child: Column(
+                  children: [
+                    const Header(),
+                    const SizedBox(height: 20),
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
                       children: [
-
-                        const Header(),
-
-                        const SizedBox(height: 20),
-
-                        FormStatus(constraints: constraints, controller: usernameController),
-                        const SizedBox(height: 10),
-
-                        /// button
-                        Row(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-
-                            BizappButton(
-                              color: Colors.green,
-                              title: "Enter",
-                              tapCallback: (){
-                                setState(() {
-                                  model.call = true;
-                                });
-                                model.loginServices(context, userid: usernameController.text).then((value) {
-                                  setState(() {
-                                    model.call = false;
-                                  });
-                                });
-                              }),
-                            const SizedBox(width: 10),
-
-                            BizappButton(
-                                color: Colors.lightBlue[400]!,
-                                title: "Paste",
-                                tapCallback: () async {
-                                  await Clipboard.getData(Clipboard.kTextPlain).then((value){
-                                    usernameController.text = value?.text ?? "";
-                                  });
-                                }),
-                            const SizedBox(width: 10),
-
-                            BizappButton(
-                                color: Colors.red[400]!,
-                                title: "Clear",
-                                tapCallback: () => usernameController.clear()
-                            ),
-
-                          ],
+                        FormStatus(
+                            constraints: constraints,
+                            controller: usernameController),
+                        IconButton(
+                          icon: Icon(Icons.search),
+                          onPressed: () async {
+                            setState(() {
+                              model.call = true;
+                            });
+                            try {
+                              await model.loginServices(context,
+                                  userid: usernameController.text);
+                            } finally {
+                              setState(() {
+                                model.call = false;
+                              });
+                            }
+                          },
                         ),
-                        const SizedBox(height: 10),
-
-                        _body2(constraints)
-
+                        const SizedBox(width: 10),
                       ],
                     ),
+                    const SizedBox(height: 10),
+                    _body2(constraints),
+                  ],
+                ),
               ),
             );
           },
         ),
       ),
+      // drawer: SideDrawer(),
     );
   }
 
-  Widget _tips(){
-    return context.read<StatusController>().tarikhtamat != "" ? const Text("* Adakah tarikh tamat backdated?\nJika ya, pakej user ni dah expired\n\n", style: TextStyle(
-      fontSize: 14, fontWeight: FontWeight.w300
-    ),) : const SizedBox();
-  }
+  Widget _body2(BoxConstraints constraints) {
 
-  Widget _body2(BoxConstraints constraints){
-    return context.read<StatusController>().call == false ? Consumer<StatusController>(
-      builder: (context, provider, child) {
-      return Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
+    String formatDate(DateTime date) {
+      String year = date.year.toString();
+      String month = date.month.toString().padLeft(2, '0');
+      String day = date.day.toString().padLeft(2, '0');
+      String hour =
+          (date.hour > 12) ? (date.hour - 12).toString() : date.hour.toString();
+      if (hour == '0') hour = '12'; // Adjust 0 to 12 for midnight
+      String minute = date.minute.toString().padLeft(2, '0');
+      String period = (date.hour >= 12) ? 'PM' : 'AM';
+      return '$day/$month/$year $hour:$minute $period';
+    }
 
-            Wrap(
-              runSpacing: 5,
-              children: [
+    DateTime now = DateTime.now();
+    String formattedDate = formatDate(now);
 
-                /// data user
-                DataUser(children: [
-                  BizappText(text: "Username:  ${provider.username}"),
-                  BizappText(text: "Nama:  ${provider.nama}"),
-                  BizappText(text: "Pakej:  ${provider.roleid}"),
-                  BizappText(text: "Tarikh naik taraf:  ${provider.tarikhnaiktaraf}"),
-                  Text("Tarikh tamat:  ${provider.tarikhtamat}", style: const TextStyle(
-                      fontSize: 16, fontWeight: FontWeight.bold, color: Colors.red
-                  )),
+    return context.read<StatusController>().call == false
+        ? Consumer<StatusController>(
+            builder: (context, provider, child) {
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      color: Colors.grey[200], // Set the background color of the box // Solid border
+                      borderRadius: BorderRadius.circular(10), // Optional: Add rounded corners
+                    ),
+                    child: Wrap(
+                      runSpacing: 5,
+                      children: [
+                        /// data user
+                        DataUser(children: [
+                          BizappText(text: "Username:  ${provider.username}"),
+                          BizappText(text: "Nama:  ${provider.nama}"),
+                          BizappText(text: "Pakej:  ${provider.roleid}"),
+                          BizappText(text: "Tarikh naik taraf:  ${provider.tarikhnaiktaraf}"),
+                          Text("Tarikh tamat:  ${provider.tarikhtamat}", style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.red)),
+                          Text("Tarikh sekarang: $formattedDate", style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.green)),
+                          // const Text("Status: Aktif",
+                          //     style: TextStyle(
+                          //         fontSize: 16,
+                          //         fontWeight: FontWeight.bold,
+                          //         color: Colors.green)),
+                          //_tips(),
+                          provider.basicplusonly != "-" &&
+                                  provider.basicplusonly != ""
+                              ? BizappText(
+                                  text:
+                                      "Basic+ Add On: ${provider.basicplusonly}\n")
+                              : const SizedBox(),
+                        ]),
+                        SizedBox(width: constraints.maxWidth >= 501 ? 50 : 10),
 
-                  _tips(),
-                  provider.basicplusonly != "-" && provider.basicplusonly != "" ?
-                  BizappText(text: "Basic+ Add On: ${provider.basicplusonly}\n") : const SizedBox(),
-
-                ]),
-
-                SizedBox(width: constraints.maxWidth >= 501 ? 50 : 10),
-
-                /// data rekod
-                provider.callDedagang == false ? DataUser(children: [
-                  BizappText(text: "Rekod Tempahan : ${provider.rekodtempahan}"),
+                        /// data rekod
+                        provider.callDedagang == false
+                            ? DataUser(children: [
+                                BizappText(
+                                    text:
+                                        "Rekod Tempahan : ${provider.rekodtempahan}"),
+                                const SizedBox(height: 10),
+                                BizappText(
+                                    text:
+                                        "Bil. Tempahan : ${provider.biltempahan}"),
+                                const SizedBox(height: 10),
+                                BizappText(
+                                    text: "Bil. Ejen : ${provider.bilEjen}"),
+                                const SizedBox(height: 10),
+                                BizappText(
+                                    text:
+                                        "Ada Bizappay : ${provider.bizappayacc}"),
+                              ])
+                            : const GetLoad(text: "Load data statistics ..."),
+                      ],
+                    ),
+                  ),
                   const SizedBox(height: 10),
-                  BizappText(text: "Bil. Tempahan : ${provider.biltempahan}"),
-                  const SizedBox(height: 10),
-                  BizappText(text: "Bil. Ejen : ${provider.bilEjen}"),
-                  BizappText(text: "Ada Bizappay : ${provider.bizappayacc}"),
-                ]) : const GetLoad(text: "Load data statistics ..."),
-
-              ],
-            ),
-
-            const SizedBox(height: 0),
-            const BizappText(text: 'Senarai rekod 5 terakhir: '),
-
-            /// data list
-            provider.callRekod == false ?
-            provider.listrekod.isEmpty ? const Text('Tiada Rekod', style: TextStyle(
-              fontSize: 16,
-            ))
-                : Center(child: DataList(listrekod: provider.listrekod))
-                : const GetLoad(text: "Load data record ...")
-
-          ],
-        );
-      },
-    )
+                  Container(
+                    constraints: BoxConstraints(maxWidth: MediaQuery.of(context).size.width * 0.4),
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      color: Colors.grey[200], 
+                      borderRadius: BorderRadius.circular(10), // Optional: Add rounded corners
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment
+                          .center, // Center align content horizontally
+                      children: [
+                        SizedBox(height: 5), // Add space above the text
+                        BizappText(text: 'Senarai rekod 5 terakhir: '),
+                        SizedBox(height: 5), // Add space below the text
+                        /// data list
+                        provider.callRekod == false
+                            ? provider.listrekod.isEmpty
+                                ? Text('Tiada Rekod',
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                    ))
+                                : Center(
+                                    child:
+                                        DataList(listrekod: provider.listrekod))
+                            : GetLoad(text: "Load data record ..."),
+                      ],
+                    ),
+                  ),
+                ],
+              );
+            },
+          )
         : const GetLoad(text: "Load data ...");
   }
-
 }
 
 class Header extends StatelessWidget {
@@ -185,14 +227,14 @@ class Header extends StatelessWidget {
   Widget build(BuildContext context) {
     return DecoratedBox(
       decoration: BoxDecoration(
-        color: Colors.grey[300],
-        borderRadius: BorderRadius.circular(80)
-      ),
+          color: Colors.white, borderRadius: BorderRadius.circular(80)),
       child: const Padding(
-        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-        child: Text("Status Renew Bizapp User", style: TextStyle(
-         fontSize: 16
-        )),
+        padding: EdgeInsets.fromLTRB(20.0, 30.0, 20.0, 10.0),
+        child: Text("Status Renew Bizapp User",
+            style: TextStyle(
+              fontSize: 32,
+              fontWeight: FontWeight.w900,
+            )),
       ),
     );
   }
@@ -201,35 +243,55 @@ class Header extends StatelessWidget {
 class FormStatus extends StatelessWidget {
   final BoxConstraints constraints;
   final TextEditingController controller;
-  const FormStatus({super.key, required this.constraints, required this.controller});
+  const FormStatus(
+      {super.key, required this.constraints, required this.controller});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: constraints.maxWidth >= 501 ? MediaQuery.sizeOf(context).width * .5 : MediaQuery.sizeOf(context).width * .9,
-      child: TextFormField(
-        style: const TextStyle(fontSize: 16),
-        keyboardType: TextInputType.emailAddress,
-        validator: (e) => e!.isEmpty ? 'username cannot empty' : null,
-        controller: controller,
-        autovalidateMode: AutovalidateMode.onUserInteraction,
-        inputFormatters: <TextInputFormatter>[
-          FilteringTextInputFormatter.allow(RegExp("[0-9a-zA-Z]")),
-        ],
-        decoration: const InputDecoration(
-          hoverColor: Colors.transparent,
-          border: OutlineInputBorder(
-            borderSide: BorderSide(
-              width: 0, style: BorderStyle.solid,
-            ),
+    return LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints constraints) {
+        return SizedBox(
+          width: constraints.maxWidth >= 501
+              ? MediaQuery.of(context).size.width * 0.3
+              : MediaQuery.of(context).size.width * 0.9,
+          child: Row(
+            children: [
+              Expanded(
+                child: TextFormField(
+                  style: const TextStyle(fontSize: 16),
+                  keyboardType: TextInputType.emailAddress,
+                  controller: controller,
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  inputFormatters: <TextInputFormatter>[
+                    FilteringTextInputFormatter.allow(RegExp("[0-9a-zA-Z]")),
+                  ],
+                  decoration: InputDecoration(
+                    hoverColor: Colors.transparent,
+                    border: OutlineInputBorder(
+                      borderRadius:
+                          BorderRadius.circular(50), // Rounded corners
+                      borderSide: const BorderSide(
+                        width: 2,
+                        style: BorderStyle.solid,
+                      ),
+                    ),
+                    filled: true,
+                    labelStyle: const TextStyle(fontSize: 14),
+                    labelText: 'Username',
+                    fillColor: Colors.white,
+                    suffixIcon: IconButton(
+                      icon: const Icon(Icons.clear, color: Colors.red),
+                      onPressed: () {
+                        controller.clear();
+                      },
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
-          filled: true,
-          labelStyle: TextStyle(fontSize: 14),
-          labelText: 'Username',
-          fillColor: Colors.white,
-        ),
-      ),
+        );
+      },
     );
   }
 }
-
