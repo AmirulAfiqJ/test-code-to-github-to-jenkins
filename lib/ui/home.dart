8c73fec517285edbbab660aa86e4cb0f5e323c73
Engku Nazurah <engkunazurah7@gmail.com>
2024-06-24 16:15:31 +0800

    home with routing

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,4 +1,10 @@
 import 'package:bizapptrack/ui/customAppBar.dart';
+import 'package:bizapptrack/ui/expiring.dart';
+import 'package:bizapptrack/ui/inactive.dart';
+import 'package:bizapptrack/ui/newRenew.dart';
+import 'package:bizapptrack/ui/others.dart';
+import 'package:bizapptrack/ui/sideNav.dart';
+import 'package:bizapptrack/ui/support.dart';
 import 'package:flutter/material.dart';
 import 'package:bizapptrack/utils/route.dart';
 import 'package:bizapptrack/viewmodel/home_viewmodel.dart';
@@ -36,68 +42,32 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       key: _scaffoldKey,
       appBar: CustomAppBar(username: viewModel.username, scaffoldKey: _scaffoldKey),
-      body: Row(
+           body: Row(
         children: [
-          Container(
-            width: MediaQuery.of(context).size.width * 0.5,
-            color: const Color.fromARGB(255, 101, 102, 102),
-            child: Stack(
-              children: [
-                Container(
-                  width: double.infinity,
-                  height: double.infinity,
-                  decoration: const BoxDecoration(
-                    image: DecorationImage(
-                      image: NetworkImage(
-                        'https://static.vecteezy.com/system/resources/previews/025/368/863/original/group-of-people-in-office-discussing-plan-graphic-illustration-ui-illustration-gui-ai-generated-png.png',
-                      ),
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ),
-                Positioned(
-                  bottom: 45.0,
-                  right: 50.0,
-                  child: Text(
-                    'Welcome ${viewModel.username}!',
-                    style: const TextStyle(
-                      fontFamily: 'Roboto',
-                      color: Color.fromARGB(255, 0, 0, 0),
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-              ],
-            ),
-          ),
+          SideDrawer(username: viewModel.username), // Add the side navigation here
           Expanded(
             child: Container(
               // color: Color.fromARGB(255, 243, 251, 255),
               child: Center(
                 child: Padding(
-                  padding: const EdgeInsets.all(16.0),
+                  padding: EdgeInsets.all(16.0),
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      const SizedBox(height: 70.0),
+                      SizedBox(height: 70.0),
                       SizedBox(
                         width: buttonWidth,
                         height: buttonHeight,
                         child: ElevatedButton.icon(
                           onPressed: () {
-                            // Navigator.push(
-                            //   context,
-                            //   MaterialPageRoute(builder: (context) => NewCustomer(username: viewModel.username)),
-                            // );
-                            Navigator.pushNamed(context, AppRoutes.newRenew, arguments: {
-                              "username": viewModel.username
-                            });
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => NewCustomer()),
+                            );
                           },
-                          icon: const Icon(Icons.fiber_new, size: 30.0), // Set icon size
-                          label: const Padding(
+                          icon: Icon(Icons.fiber_new, size: 30.0), // Set icon size
+                          label: Padding(
                             padding: EdgeInsets.symmetric(vertical: 16.0),
                             child: Text(
                               'New & Renew',
@@ -106,29 +76,26 @@ class _HomePageState extends State<HomePage> {
                           ),
                           style: ElevatedButton.styleFrom(
                             foregroundColor: Colors.black,
-                            backgroundColor: const Color.fromARGB(255, 173, 185, 242),
+                            backgroundColor: Color.fromARGB(255, 173, 185, 242),
                             shape: RoundedRectangleBorder(
                               borderRadius: BorderRadius.circular(10.0),
                             ), // Text color
                           ),
                         ),
                       ),
-                      const SizedBox(height: 30.0),
+                      SizedBox(height: 30.0),
                       SizedBox(
                         width: buttonWidth,
                         height: buttonHeight,
                         child: ElevatedButton.icon(
                           onPressed: () {
-                            // Navigator.push(
-                            //   context,
-                            //   MaterialPageRoute(builder: (context) => Inactive(username: widget.username)),
-                            // );
-                            Navigator.pushNamed(context, AppRoutes.inactive, arguments: {
-                            "username": viewModel.username
-                          });
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => Inactive()),
+                            );
                           },
-                          icon: const Icon(Icons.remove_circle_outline, size: 30.0), // Set icon size
-                          label: const Padding(
+                          icon: Icon(Icons.remove_circle_outline, size: 30.0), // Set icon size
+                          label: Padding(
                             padding: EdgeInsets.symmetric(vertical: 16.0),
                             child: Text(
                               'Inactive',
@@ -137,60 +104,54 @@ class _HomePageState extends State<HomePage> {
                           ),
                           style: ElevatedButton.styleFrom(
                             foregroundColor: Colors.black,
-                            backgroundColor: const Color.fromARGB(255, 255, 228, 138),
+                            backgroundColor: Color.fromARGB(255, 255, 228, 138),
                             shape: RoundedRectangleBorder(
                               borderRadius: BorderRadius.circular(10.0),
                             ), // Text color
                           ),
                         ),
                       ),
-                      const SizedBox(height: 30.0),
-                      SizedBox(
-                        width: buttonWidth,
-                        height: buttonHeight,
-                      child: ElevatedButton.icon(
-                        onPressed: () {
-                          // Navigator.push(
-                          //   context,
-                          //   MaterialPageRoute(builder: (context) => Expiring(username: widget.username)),
-                          // );
-                          Navigator.pushNamed(context, AppRoutes.expiring, arguments: {
-                              "username": viewModel.username
-                            });
-                        },
-                        icon: const Icon(Icons.access_time, size: 30.0), // Set icon size
-                        label: const Padding(
-                          padding: EdgeInsets.symmetric(vertical: 16.0),
-                          child: Text(
-                            'Expiring',
-                            style: TextStyle(fontSize: 14.0), // Set button text size
-                          ),
-                        ),
-                        style: ElevatedButton.styleFrom(
-                          foregroundColor: Colors.black,
-                          backgroundColor: const Color.fromARGB(255, 255, 172, 172),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10.0),
-                          ), // Text color
-                        ),
-                      ),
-                      ),
-                      const SizedBox(height: 30.0),
+                      SizedBox(height: 30.0),
                       SizedBox(
                         width: buttonWidth,
                         height: buttonHeight,
                         child: ElevatedButton.icon(
                           onPressed: () {
-                            // Navigator.push(
-                            //   context,
-                            //   MaterialPageRoute(builder: (context) => Others(username: widget.username)),
-                            // );
-                            Navigator.pushNamed(context, AppRoutes.others, arguments: {
-                            "username": viewModel.username
-                            });
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => Expiring()),
+                            );
                           },
-                          icon: const Icon(Icons.category, size: 30.0), // Set icon size
-                          label: const Padding(
+                          icon: Icon(Icons.access_time, size: 30.0), // Set icon size
+                          label: Padding(
+                            padding: EdgeInsets.symmetric(vertical: 16.0),
+                            child: Text(
+                              'Expiring',
+                              style: TextStyle(fontSize: 14.0), // Set button text size
+                            ),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                            foregroundColor: Colors.black,
+                            backgroundColor: Color.fromARGB(255, 255, 172, 172),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                            ), // Text color
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 30.0),
+                      SizedBox(
+                        width: buttonWidth,
+                        height: buttonHeight,
+                        child: ElevatedButton.icon(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => Others()),
+                            );
+                          },
+                          icon: Icon(Icons.category, size: 30.0), // Set icon size
+                          label: Padding(
                             padding: EdgeInsets.symmetric(vertical: 16.0),
                             child: Text(
                               'Others',
@@ -206,22 +167,19 @@ class _HomePageState extends State<HomePage> {
                           ),
                         ),
                       ),
-                      const SizedBox(height: 30.0),
+                      SizedBox(height: 30.0),
                       SizedBox(
                         width: buttonWidth,
                         height: buttonHeight,
                         child: ElevatedButton.icon(
                           onPressed: () {
-                            // Navigator.push(
-                            //   context,
-                            //   MaterialPageRoute(builder: (context) => SupportPage(username: widget.username)),
-                            // );
-                            Navigator.pushNamed(context, AppRoutes.support, arguments: {
-                            "username": viewModel.username
-                          });
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => SupportPage()),
+                            );
                           },
-                          icon: const Icon(Icons.support_agent, size: 30.0), // Set icon size
-                          label: const Padding(
+                          icon: Icon(Icons.support_agent, size: 30.0), // Set icon size
+                          label: Padding(
                             padding: EdgeInsets.symmetric(vertical: 16.0),
                             child: Text(
                               'Support',
@@ -230,7 +188,7 @@ class _HomePageState extends State<HomePage> {
                           ),
                           style: ElevatedButton.styleFrom(
                             foregroundColor: Colors.black,
-                            backgroundColor: const Color.fromARGB(255, 112, 211, 101),
+                            backgroundColor: Color.fromARGB(255, 112, 211, 101),
                             shape: RoundedRectangleBorder(
                               borderRadius: BorderRadius.circular(10.0),
                             ), // Text color
